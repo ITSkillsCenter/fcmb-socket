7a42e005844e6a0f900b97ff929af588c4c1914f
Opeyemi Toheeb <68414299+logiccamp@users.noreply.github.com>
2024-09-28 00:50:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+  app.disable("x-powered-by");
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Authorization, Origin, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
